c3d1877fd36f4b83275b2ab8ff9023d3049446a6
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-05-17 14:54:45 +0300

    Обновление update.php

--- update.php
+++ update.php
@@ -4,10 +4,13 @@
  */
 
 define('BX_SRC_URL', 'https://github.com/gleb-mihalkov/bitrix/archive/master.zip');
-define('BX_SRC_DIR', './bitrix-master');
+define('BX_SRC_DIR', 'bitrix-master');
+
+$path = dirname(__FILE__);
+$src_path = $path.'/'.trim(BX_SRC_DIR, '/');
 
 $file_raw = file_get_contents(BX_SRC_URL);
-$file_name = tempnam('.', 'bx_');
+$file_name = tempnam($path, 'bx_');
 file_put_contents($file_name, $file_raw);
 
 $zip = new ZipArchive();
@@ -16,16 +19,16 @@ $zip->extractTo('.');
 $zip->close();
 unlink($file_name);
 
-$dir = opendir(BX_SRC_DIR);
+$src_path = opendir(BX_SRC_DIR);
 
-while ($item = readdir($dir)) {
+while ($item = readdir($src_path)) {
   if ($item === '.' || $item === '..') continue;
-  $item_old = BX_SRC_DIR.'/'.$item;
-  $item_new = './'.$item;
+  $item_old = $src_path.'/'.$item;
+  $item_new = $path.'/'.$item;
   rename($item_old, $item_new);
 }
 
-closedir($dir);
-rmdir(BX_SRC_DIR);
+closedir($src_path);
+rmdir($src_path);
 
 echo 'Library successfully updated.'.PHP_EOL;
