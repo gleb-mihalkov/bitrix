3ace156ca796ddc62c7caf8e5060bec24ace7b39
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-05-17 16:24:07 +0300

    Обновление update.pph

--- update.php
+++ update.php
@@ -2,24 +2,27 @@
 /**
  * Скрипт, автоматически обновляющий библиотеку из репозитория Github.
  */
-
-define('BX_SRC_URL', 'https://github.com/gleb-mihalkov/bitrix/archive/master.zip');
+define('BX_SRC_URL', 'https://github.com/gleb-mihalkov/bitrix/archive/master.tar.gz');
 define('BX_SRC_DIR', 'bitrix-master');
 
 $path = dirname(__FILE__);
 $src_path = $path.'/'.trim(BX_SRC_DIR, '/');
 
-$file_raw = file_get_contents(BX_SRC_URL);
 $file_name = tempnam($path, 'bx_');
+$file_name_zip = $file_name.'.tar.gz';
+rename($file_name, $file_name_zip);
+$file_name = $file_name_zip;
+
+$file_raw = file_get_contents(BX_SRC_URL);
 file_put_contents($file_name, $file_raw);
 
-$zip = new ZipArchive();
-$zip->open($file_name);
-$zip->extractTo('.');
-$zip->close();
+$success = false;
+$cmd = 'cd '.$path.' && tar -xvf '.basename($file_name);
+$cmd_int = null;
+$cmd_out = system($cmd, $cmd_int);
 unlink($file_name);
 
-$src = opendir(BX_SRC_DIR);
+$src = opendir($src_path);
 
 while ($item = readdir($src)) {
   if ($item === '.' || $item === '..') continue;
