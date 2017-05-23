389b2e6e16e9a83e6ad5e110e2a2b81dfae576cd
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-05-23 12:25:51 +0300

    Переименование и подключение файла с фильтрами.

--- include.php
+++ include.php
@@ -3,6 +3,7 @@
  * Набор дополнительных функций для работы с ядром Битрикс.
  */
 
+require __DIR__.'/filter.php';
 require __DIR__.'/result.php';
 require __DIR__.'/result_highload.php';
 require __DIR__.'/highload.php';
