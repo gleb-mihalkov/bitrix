1304f721618974faa8fc30aa780c1c0e326fb81d
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-05-29 00:56:57 +0300

    Добавлен фильтр получения нужной формы слова для числа.

--- filter.php
+++ filter.php
@@ -16,3 +16,22 @@ function bx_fl_number($number, $precision = 2, $float_separator = ',', $division
   $text = number_format($number, $precision, $float_separator, $division_separator);
   return $text;
 }
+
+/**
+ * Возвращает слово, относящееся к числу, в нужной форме.
+ * @param  integer $number Число.
+ * @param  string $one     Форма слова для единицы. Пример: один стол.
+ * @param  string $two     Форма слова для двойки. Пример: два стола.
+ * @param  string $five    Форма слова для пяти. Пример: пять столов.
+ * @return string          Форма слова, соответствующая указанному числу.
+ */
+function bx_fl_words($number, $one, $two, $five) {
+  $number = intval($number);
+  if ($number >= 11 && $number <= 14) return $five;
+
+  $number = $number % 10;
+  if ($number >= 2 && $number <= 4) return $two;
+  if ($number == 1) return $one;
+
+  return $five;
+}
